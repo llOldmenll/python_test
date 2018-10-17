555aa284b5206eaafc03015e0c69d1d622d46480
llOldmenll <shark7733@gmail.com>
2018-10-17 21:45:17 +0300

    - check button added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -1,12 +1,10 @@
 import tkinter
-from tkinter import messagebox
-
-
-def ShowPopUp():
-    msg = messagebox.showinfo("Test title", "My unique message")
-
+from tkinter import *
 
 window = tkinter.Tk()
-buttonWidget = tkinter.Button(window, text="Click me", command=ShowPopUp)
-buttonWidget.place(x=25, y=100)
+
+cv = IntVar()
+cb = Checkbutton(window, text="Click Me", variable=cv, onvalue=1, offvalue=0, height=30, width=20, bg="purple")
+cb.pack()
+
 window.mainloop()
