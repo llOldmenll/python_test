0d51380bc97c7d61842cd847dd8c6cd1348ed002
llOldmenll <shark7733@gmail.com>
2018-10-17 22:14:02 +0300

    - menu bar added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -3,28 +3,17 @@ from tkinter import *
 
 window = tkinter.Tk()
 
-str = StringVar()
-label = Label(window, textvariable=str)
-str.set("Amazing test Label")
-label.pack()
+menubar = Menu(window)
 
-frame = Frame(window)
-frame.pack()
+menu1 = Menu(menubar)
+menu1.add_command(label="New")
+menu1.add_separator()
+menu1.add_command(label="Exit", command=window.quit())
+menubar.add_cascade(label="Section1", menu=menu1)
 
-buttonWidget = tkinter.Button(window, text="Click me")
-buttonWidget.pack(side=LEFT)
+menu2 = Menu(menubar)
+menu2.add_command(label="Add")
+menubar.add_cascade(label="Section2", menu=menu2)
 
-cv = IntVar()
-cb = Checkbutton(window, text="Click Me", variable=cv, onvalue=1, offvalue=0, height=30, width=20, bg="purple")
-cb.pack()
-
-list = Listbox(window)
-list.insert(0, "Item 0")
-list.insert(1, "Item 1")
-list.insert(2, "Item 2")
-list.insert(3, "Item 3")
-list.insert(4, "Item 4")
-list.insert(5, "Item 5")
-list.pack()
-
+window.config(menu=menubar)
 window.mainloop()
