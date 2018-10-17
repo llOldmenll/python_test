2c5ce71b41aebae33f029cc55ae099b41ebba8c9
llOldmenll <shark7733@gmail.com>
2018-10-17 21:53:39 +0300

    - frame added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -3,6 +3,12 @@ from tkinter import *
 
 window = tkinter.Tk()
 
+frame = Frame(window)
+frame.pack()
+
+buttonWidget = tkinter.Button(window, text="Click me")
+buttonWidget.pack(side=LEFT)
+
 cv = IntVar()
 cb = Checkbutton(window, text="Click Me", variable=cv, onvalue=1, offvalue=0, height=30, width=20, bg="purple")
 cb.pack()
