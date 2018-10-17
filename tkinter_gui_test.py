bf93aaaacc5993c4b6bccd985bedd441bf2dca3a
llOldmenll <shark7733@gmail.com>
2018-10-17 21:30:20 +0300

    - button and pop up window added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -1,4 +1,12 @@
 import tkinter
+from tkinter import messagebox
+
+
+def ShowPopUp():
+    msg = messagebox.showinfo("Test title", "My unique message")
+
 
 window = tkinter.Tk()
+buttonWidget = tkinter.Button(window, text="Click me", command=ShowPopUp)
+buttonWidget.place(x=25, y=100)
 window.mainloop()
