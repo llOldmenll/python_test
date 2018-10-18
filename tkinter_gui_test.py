2b3102c7b654d837747e8ebf6ba61efff7e0aae0
llOldmenll <shark7733@gmail.com>
2018-10-18 20:24:02 +0300

    - scale widget added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -2,26 +2,19 @@ import tkinter
 from tkinter import *
 
 
-def select():
-    selection = "Awesome option " + str(var.get())
-    label.config(text=selection)
+def getValue():
+    value = "Value: " + str(var.get())
+    label.config(text=value)
 
 
 window = tkinter.Tk()
 
-var = IntVar()
+var = DoubleVar()
+scale = Scale(window, variable=var)
+scale.pack()
 
-radio1 = Radiobutton(window, text="Option1", variable=var, value=1, command=select)
-radio1.pack(anchor=W)
-
-radio2 = Radiobutton(window, text="Option2", variable=var, value=2, command=select)
-radio2.pack(anchor=W)
-
-radio3 = Radiobutton(window, text="Option3", variable=var, value=3, command=select)
-radio3.pack(anchor=W)
-
-radio4 = Radiobutton(window, text="Option4", variable=var, value=4, command=select)
-radio4.pack(anchor=W)
+btn = Button(window, text="Receive value", command=getValue)
+btn.pack()
 
 label = Label(window)
 label.pack()
