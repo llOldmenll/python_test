db8c71cbc92d4938fe9cdf3a2fa3b92b18884670
llOldmenll <shark7733@gmail.com>
2018-10-18 20:34:24 +0300

    - scrollbar widget added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -1,22 +1,16 @@
 import tkinter
 from tkinter import *
 
-
-def getValue():
-    value = "Value: " + str(var.get())
-    label.config(text=value)
-
-
 window = tkinter.Tk()
 
-var = DoubleVar()
-scale = Scale(window, variable=var)
-scale.pack()
+scrollBar = Scrollbar(window)
+scrollBar.pack(side=RIGHT, fill=Y)
 
-btn = Button(window, text="Receive value", command=getValue)
-btn.pack()
+list = Listbox(window, yscrollcommand=scrollBar.set)
+for line in range(1000):
+    list.insert(END, "Row: " + str(line + 1))
+list.pack()
 
-label = Label(window)
-label.pack()
+scrollBar.config(command=list.yview)
 
 window.mainloop()
