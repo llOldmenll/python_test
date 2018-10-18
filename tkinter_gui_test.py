c385b0661e5c3b1a2c0bcb05bd210bf617478ab2
llOldmenll <shark7733@gmail.com>
2018-10-18 20:41:52 +0300

    - top level window added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -2,16 +2,8 @@ import tkinter
 from tkinter import *
 
 window = tkinter.Tk()
+window.title("Main")
 
-text = Text(window)
-text.insert(INSERT, "Hello ")
-text.insert(END, "BYE, BYE.....")
-text.pack()
-
-text.tag_add("here", "1.0", "1.4")
-text.tag_add("start", "1.8", "1.13")
-
-text.tag_config("here", background="yellow", foreground="blue")
-text.tag_config("start", background="grey", foreground="white")
-
-window.mainloop()
+topL = Toplevel()
+topL.title("Child")
+topL.mainloop()
