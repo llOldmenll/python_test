38c2d8a77fac9b87041cd3e52944574012a82028
llOldmenll <shark7733@gmail.com>
2018-10-18 20:59:46 +0300

    - message box added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -1,21 +1,15 @@
 import tkinter
 from tkinter import *
+from tkinter import messagebox
 
 window = tkinter.Tk()
 
-m1 = PanedWindow(window)
-m1.pack(fill=BOTH, expand=1)
 
-left = Entry(m1, bd=5)
-m1.add(left)
+def showMessage():
+    messagebox.showinfo("Title", "Message")
 
-m2 = PanedWindow(m1, orient=VERTICAL)
-m1.add(m2)
 
-top = Scale(m2, orient=HORIZONTAL)
-m2.add(top)
-
-btn = Button(m2, text="Click me")
-m2.add(btn)
+btn = Button(window, text="Click me", command=showMessage)
+btn.pack()
 
 window.mainloop()
