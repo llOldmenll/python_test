2046ea41b2d96a4e252f3fbe97f1719b4ea462f2
llOldmenll <shark7733@gmail.com>
2018-10-18 20:51:30 +0300

    - paned window widget added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -3,7 +3,19 @@ from tkinter import *
 
 window = tkinter.Tk()
 
-spin = Spinbox(window, from_=0, to=10)
-spin.pack()
+m1 = PanedWindow(window)
+m1.pack(fill=BOTH, expand=1)
+
+left = Entry(m1, bd=5)
+m1.add(left)
+
+m2 = PanedWindow(m1, orient=VERTICAL)
+m1.add(m2)
+
+top = Scale(m2, orient=HORIZONTAL)
+m2.add(top)
+
+btn = Button(m2, text="Click me")
+m2.add(btn)
 
 window.mainloop()
