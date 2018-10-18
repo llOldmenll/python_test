ecb592401a0078cace66df89726f280a2b2ad377
llOldmenll <shark7733@gmail.com>
2018-10-18 20:08:52 +0300

    - message added

--- tkinter_gui_test.py
+++ tkinter_gui_test.py
@@ -3,17 +3,9 @@ from tkinter import *
 
 window = tkinter.Tk()
 
-menubar = Menu(window)
+str = StringVar()
+str.set("The huge message ever ever ever ever...")
+msg = Message(window, textvariable=str)
+msg.pack()
 
-menu1 = Menu(menubar)
-menu1.add_command(label="New")
-menu1.add_separator()
-menu1.add_command(label="Exit", command=window.quit())
-menubar.add_cascade(label="Section1", menu=menu1)
-
-menu2 = Menu(menubar)
-menu2.add_command(label="Add")
-menubar.add_cascade(label="Section2", menu=menu2)
-
-window.config(menu=menubar)
 window.mainloop()
